7d8ef5eba1515361e32e849038d3993c0e4fc6a5
= <abhisingh30800@gmail.com>
2024-04-15 19:43:20 +0530

    removed console.log from cloudinary upload file function

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -11,7 +11,6 @@ cloudinary.config({
 const uploadOnCloudinary = async (localFilePath) => {
   try {
     if (!localFilePath) return null;
-    console.log(typeof process.env.CLOUD_API_KEY);
     //upload the file on cloudinary
     const response = await cloudinary.uploader.upload(localFilePath, {
       resource_type: "auto",
