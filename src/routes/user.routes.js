b1d6c0038341de5d67620f28721f89cc216fd15c
= <abhisingh30800@gmail.com>
2024-04-17 11:55:57 +0530

    added routes for update cover & avatar image

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -6,6 +6,8 @@ import {
   refreshAccessToken,
   getCurrentUser,
   changeCurrentUserPassword,
+  updateUserAvatar,
+  updateUserCoverImage,
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
@@ -29,6 +31,12 @@ router.route("/login").post(loginUser);
 router.route("/logout").post(verifyJWT, logoutUser);
 router.route("/refresh-token").post(refreshAccessToken);
 router.route("/changePass").post(verifyJWT, changeCurrentUserPassword);
+router
+  .route("/avatar")
+  .post(verifyJWT, upload.single("avatar"), updateUserAvatar);
+router
+  .route("/cover-image")
+  .post(verifyJWT, upload.single("coverImage"), updateUserCoverImage);
 router.route("/currentUser").get(verifyJWT, getCurrentUser);
 
 export default router;
